c6a29ec0f72c35b736601ca818b0dfa5ea14bebe
Diego Alves Openheimer <Diego.Openheimer@pa.pref>
2017-10-10 15:56:55 -0300

    Add Cadastrar.ejs and update CSS

--- SistemaTransporte/model/auth.js
+++ SistemaTransporte/model/auth.js
@@ -2,7 +2,7 @@ const md5 = require('md5')
 const findOneAdm = (connection, req) => {
 
     const senha = md5(req.body.password)
-    console.log(senha)
+
     return new Promise(async(resolve, reject) => {
 
         await connection.query(`SELECT * FROM administrador WHERE username = '${req.body.usuario}' AND senha = '${senha}'`, (err, result) => {
